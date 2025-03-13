2475425381c6db8b3f75b0d4f2518dbcdc907dbd
Srihari Kulkarni <156205103+Srihari3435@users.noreply.github.com>
2025-03-13 16:40:33 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello\n";
   cout << "Hello, This is Srihari,JK PES1UG22CS610\n";
   cout << "I have successfully built and run this  \n";
   return 0;
